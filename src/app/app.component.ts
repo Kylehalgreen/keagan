10c72ef944ce9f632931a46bf53ad12e029c53aa
Kyle <kylehalgreen5@gmail.com>
2025-03-17 13:20:20 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 @Component({
   standalone: true,
   selector: 'app-root',
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-first',
+  standalone: true,
   imports: [],
   templateUrl: './first.component.html',
   styleUrl: './first.component.css'
